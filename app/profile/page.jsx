857786bbeb316c6d59f8097371ec31f1cbdd93ca
Halil Enes Özdemir <halil.enes.ozdemir@gmail.com>
2023-07-09 20:08:37 +0300

    Feed Page completed, it's time to Profile page, btw added project images mobile and desktop

---
+++ app/profile/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+function Profile() {
+  return <div>Profile</div>;
+}
+
+export default Profile;
