ac1e075476647289b7426554b97c598166afde31
Halil Enes Özdemir <halil.enes.ozdemir@gmail.com>
2023-07-09 21:34:14 +0300

    Project finished.

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+
 import PromptCard from "./PromptCard";
 
-function PromptCardList({ data, handleTagClick }) {
+const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
       {data.map((post) => (
@@ -15,22 +16,54 @@ function PromptCardList({ data, handleTagClick }) {
       ))}
     </div>
   );
-}
+};
 
-function Feed() {
+const Feed = () => {
+  const [allPosts, setAllPosts] = useState([]);
+
+  // Search states
   const [searchText, setSearchText] = useState("");
-  const [posts, setPosts] = useState([]);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
 
-  function handleSearchChange() {}
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setAllPosts(data);
+  };
 
   useEffect(() => {
-    async function fetchPosts() {
-      const response = await fetch("/api/prompt");
-      const data = await response.json();
-      setPosts(data);
-    }
     fetchPosts();
   }, []);
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) => regex.test(item.creator.username) || regex.test(item.tag) || regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -43,12 +76,21 @@ function Feed() {
           className="search_input peer"
         />
       </form>
-      <PromptCardList
-        data={posts}
-        handleTagClick={() => {}}
-      />
+
+      {/* All Prompts */}
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList
+          data={allPosts}
+          handleTagClick={handleTagClick}
+        />
+      )}
     </section>
   );
-}
+};
 
 export default Feed;
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-function Form({ type, post, setPost, submitting, handleSubmit }) {
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
       <h1 className="head_text text-left">
@@ -8,7 +8,7 @@ function Form({ type, post, setPost, submitting, handleSubmit }) {
       </h1>
       <p className="desc text-left max-w-md">
         {type} and share amazing prompts with the world, and let your imagination run wild with any
-        AI-powered platform.
+        AI-powered platform
       </p>
 
       <form
@@ -16,27 +16,30 @@ function Form({ type, post, setPost, submitting, handleSubmit }) {
         className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism">
         <label>
           <span className="font-satoshi font-semibold text-base text-gray-700">Your AI Prompt</span>
+
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder="Write your post here"
+            required
+            className="form_textarea "
+          />
         </label>
-        <textarea
-          value={post.prompt}
-          onChange={(e) => setPost({ ...post, prompt: e.target.value })}
-          placeholder="Write your prompt here"
-          className="form_textarea"
-        />
 
         <label>
           <span className="font-satoshi font-semibold text-base text-gray-700">
-            Tag {` `}
-            <span className="font-normal">(#product, #webdevelopment, #idea)</span>
+            Field of Prompt{" "}
+            <span className="font-normal">(#product, #webdevelopment, #idea, etc.)</span>
           </span>
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            type="text"
+            placeholder="#Tag"
+            required
+            className="form_input"
+          />
         </label>
-
-        <input
-          value={post.tag}
-          onChange={(e) => setPost({ ...post, tag: e.target.value })}
-          placeholder="#tag"
-          className="form_input"
-        />
 
         <div className="flex-end mx-3 mb-5 gap-4">
           <Link
@@ -49,12 +52,12 @@ function Form({ type, post, setPost, submitting, handleSubmit }) {
             type="submit"
             disabled={submitting}
             className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white">
-            {submitting ? `${type}...` : type}
+            {submitting ? `${type}ing...` : type}
           </button>
         </div>
       </form>
     </section>
   );
-}
+};
 
 export default Form;
